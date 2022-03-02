97fe1189e3a7a449972595eddb923f671f210f3a
Rafael <rls.rafaelsouza@gmail.com>
2022-03-02 09:46:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+##Instruções de uso:
+Ao iniciar o programa, voce tem 5 segundos por padrão para apontar o seu mouse para o botão de Escrever o email do GMAIL.
+Este tempo pode ser alterado dentro do arquivo _main.py_ na linha 84 na variavel "time_start_in_seconds", em segunos.
+
+###Passo-a-passo de execução
+**DICA:** Deixe o gmail ja aberto na tela inicial, onde se encontra o botão de ESCREVER email.
+1. Inicie a execução do arquivo _main.py_
+2. Abra o navegador no gmail
+3. Posicione o mouse sobre o botão de ESCREVER email e aguarde o inicio.
---
+++ logger.py
@@ -0,0 +1,20 @@
+import datetime
+import logging
+import os
+
+if not os.path.exists('../logs'):
+    os.makedirs('../logs')
+
+home_dir = os.getcwd()
+log_name = 'logs/' + str(datetime.date.today()) + '.log'
+log_file = os.path.join(home_dir, log_name)
+
+logging.basicConfig(
+    handlers=[logging.FileHandler(filename=log_name, mode='a', encoding='utf-8')],
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)-6s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+LAYOUT_LOGS = "-" * 100
+LAYOUT_LOGS_COMUM = "=" * 70
